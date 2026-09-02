3e5ec849605756128036f509a03339905da3d47a
Anurag Patel <ap87388674@gmail.com>
2026-09-03 01:17:19 +0530

    feat: add Construct Uniform Parity Array I solution

--- algorithms/construct-uniform-parity-array-i/solution.py
+++ algorithms/construct-uniform-parity-array-i/solution.py
@@ -1,19 +1,19 @@
 class Solution:
-    def minElement(self, nums: List[int]) -> int:
-        l=[]
-        for i in nums:
-            l.append((str(i)))
-        new=[]
-        for j in l:
-            new.append(list(j))
-        new1=[]
-        for k in new:
-             new1.append(sum(int(x) for x in k))
-
-        return min(new1)
-            
-
-            
+    def uniformArray(self, nums1: list[int]) -> bool:
         
-
-        
+        j=1
+        check=True
+        for i in range(len(nums1)-1):
+            if nums1[i]%2==0:
+                if (nums1[i]- nums1[j])%2==0:
+                    j+=1
+                    pass
+            elif nums1[i]%2!=0:
+                if (nums1[i]- nums1[j])%2!=0:
+                    j+=1
+                    pass
+            else:
+                check=False
+        if check :
+            return True
+        return False
