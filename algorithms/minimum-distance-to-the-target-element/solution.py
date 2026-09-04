e52acc1eff8cbd6dc67dd00b7d5c3f60bb414e00
Anurag Patel <ap87388674@gmail.com>
2026-09-05 01:40:25 +0530

    feat: add Minimum Distance to the Target Element solution

--- algorithms/minimum-distance-to-the-target-element/solution.py
+++ algorithms/minimum-distance-to-the-target-element/solution.py
@@ -1,11 +1,8 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        l1=[[1]]
-        
-        for i in range(1,numRows):
-            l2=[1]
-            for j in range(i-1):
-                l2.append(l1[i-1][j]+ l1[i-1][j+1])
-            l2.append(1)    
-            l1.append(l2)
-        return l1
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        a=float(inf)
+        for i in range(len(nums)):
+            if nums[i]==target:
+                if a> abs(i-start):
+                    a= abs(i-start)
+        return a    
