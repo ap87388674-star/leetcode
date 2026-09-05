6972aca550acc767f595afb9393f03cfc9257181
Anurag Patel <ap87388674@gmail.com>
2026-09-05 20:24:16 +0530

    feat: add Smallest Stable Index I solution

--- algorithms/smallest-stable-index-i/solution.py
+++ algorithms/smallest-stable-index-i/solution.py
@@ -1,8 +1,16 @@
 class Solution:
-    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
-        a=float(inf)
-        for i in range(len(nums)):
-            if nums[i]==target:
-                if a> abs(i-start):
-                    a= abs(i-start)
-        return a    
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n= len(nums)
+        max_value= float('inf')
+        score= float('inf')
+        min_value= float('-inf')
+        for i in range(n):
+            max_value= max(nums[0:i+1])
+            min_value= min(nums[i: n])
+            score= max_value-min_value
+            if score<=k:
+                return i
+        return -1
+
+
+    
