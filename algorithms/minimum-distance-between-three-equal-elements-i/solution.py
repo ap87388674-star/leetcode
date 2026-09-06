49dcf77658a1ef1bca5608aa9772f8a702745bf8
Anurag Patel <ap87388674@gmail.com>
2026-09-07 01:44:30 +0530

    feat: add Minimum Distance Between Three Equal Elements I solution

--- algorithms/minimum-distance-between-three-equal-elements-i/solution.py
+++ algorithms/minimum-distance-between-three-equal-elements-i/solution.py
@@ -1,16 +1,29 @@
 class Solution:
-    def firstStableIndex(self, nums: list[int], k: int) -> int:
-        n= len(nums)
-        max_value= float('inf')
-        score= float('inf')
-        min_value= float('-inf')
-        for i in range(n):
-            max_value= max(nums[0:i+1])
-            min_value= min(nums[i: n])
-            score= max_value-min_value
-            if score<=k:
-                return i
-        return -1
+    def minimumDistance(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        if max(d.values()) <3:
+            return -1
+        else:
+            l1=[]
+            a=None
+            for key,value in d.items():
+                if value>=3:
+                    a= key
+                l=[]
+                for j  in range(len(nums)):
+                    if nums[j]==a:
+                        l.append(j)
 
+                for i in range(len(l)-2):
+                    score=(abs(l[i]-l[i+1])
+                    + abs(l[i+1]- l[i+2])
+                    + abs(l[i]-l[i+2]))
+                    l1.append(score)
+            return(min(l1))
 
-    
+        
