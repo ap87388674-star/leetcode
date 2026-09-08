93c82088fb0606c61244a29526aa9f05dbc74503
Anurag Patel <ap87388674@gmail.com>
2026-09-09 00:04:38 +0530

    feat: add Count Commas in Range solution

---
+++ algorithms/count-commas-in-range/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        s= str(n)
+        count=0
+        if len(s)<4:
+            return count
+        else:
+            count=n-1000+1
+        
+            return count
+ 
+
+
+
+                
+
+
+
+        
