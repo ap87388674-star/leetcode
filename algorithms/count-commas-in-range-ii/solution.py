bb5a03b3339fe2e4e32e3023f84e96c36bddf5d1
Anurag Patel <ap87388674@gmail.com>
2026-09-09 19:07:22 +0530

    feat: add Count Commas in Range II solution

--- algorithms/count-commas-in-range-ii/solution.py
+++ algorithms/count-commas-in-range-ii/solution.py
@@ -1,19 +1,10 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        s= str(n)
-        count=0
-        if len(s)<4:
-            return count
-        else:
-            count=n-1000+1
-        
-            return count
- 
+        count = 0
+        power = 1000
 
+        while power <= n:
+            count += n - power + 1
+            power *= 1000
 
-
-                
-
-
-
-        
+        return count
