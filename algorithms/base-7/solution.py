d5b4c711f1e1a8ded5c88eb69ebb50c225c66f9c
Anurag Patel <ap87388674@gmail.com>
2026-09-11 03:54:07 +0530

    feat: add Base 7 solution

--- algorithms/base-7/solution.py
+++ algorithms/base-7/solution.py
@@ -1,10 +1,22 @@
 class Solution:
-    def countCommas(self, n: int) -> int:
-        count = 0
-        power = 1000
+    def convertToBase7(self, num: int) -> str:
+        s=""
+        negative=num<0
+        num=abs(num)
+    
+        while (num)>=7:
+            
+            a= num//7
+            num =num%7
+            s+=str(num)
+            num=a
+        
 
-        while power <= n:
-            count += n - power + 1
-            power *= 1000
+        if negative:
+            return "-"+ str(num)+ s[::-1]
+        return str(num) + s[::-1]
 
-        return count
+
+
+
+        
